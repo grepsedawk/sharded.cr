4cb5d3e5cb0975fcf06b34a2a8ceafd783bd20b3
Alex Piechowski <alex@piechowski.io>
2022-03-11 14:36:29 -0600

    Fix spectator install
    After changing the way the template works, this was broken
    This should fix this by simply adding spectator in

--- spec/shard_jumpstart_spec.cr
+++
@@ -1,7 +0,0 @@
-require "./spec_helper"
-
-Spectator.describe ShardJumpstart do
-  it "has a version number" do
-    expect(ShardJumpstart::VERSION).not_to be nil
-  end
-end
--- spec/spec_helper.cr
+++
@@ -1,2 +0,0 @@
-require "../src/shard_jumpstart"
-require "spectator"
